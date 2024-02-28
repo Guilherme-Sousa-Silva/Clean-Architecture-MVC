f3cda00581259f3c03f685726c68fa21cf298125
Bowie <marketing@letscode.com.br>
2024-02-28 12:01:40 -0300

    Implementação da camada de InfraData, criação do context e criação dos repositories concretos

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+	public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			// chave primária
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Name)
+				.IsRequired()
+				.HasColumnType("VARCHAR")
+				.HasMaxLength(60);
+
+			// propriedade para popular as tabelas na hora de rodar as migrations
+			builder.HasData(
+				new Category(1, "Material escolar"),
+				new Category(2, "Eletrônicos"),
+				new Category(3, "Acessórios")
+				);
+		}
+	}
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+	public class ProductConfiguration : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			// chave primária
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Name)
+				.IsRequired()
+				.HasColumnType("VARCHAR")
+				.HasMaxLength(60);
+
+			builder.Property(x => x.Description)
+				.IsRequired()
+				.HasColumnType("VARCHAR")
+				.HasMaxLength(150);
+
+			builder.Property(x => x.Price)
+				.IsRequired()
+				.HasPrecision(10, 2);
+
+			builder.Property(x => x.Image)
+				.HasDefaultValue(null)
+				.HasMaxLength(250);
+
+			// mapeamento um para muitos
+			builder.HasOne(x => x.Category)
+				.WithMany(x => x.Products)
+				.HasForeignKey(x => x.Category.Id);
+		}
+	}
+}
